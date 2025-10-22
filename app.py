20b41c2b9954c96c013ed9c85bff27a41438f276
Natália <smachadonatalia@gmail.com>
2025-10-22 20:20:32 -0300

    html integration prototype

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from selenium.webdriver.common.by import By
 
+import methods
 import environment
-import constants
 
 app = Flask(__name__)
 
@@ -20,18 +20,18 @@ def run_crawler():
     try: 
         data = request.get_json() or {}
         url = data.get('url', '')
-        constants.url = url
+        environment.url = url
         
-        driver = environment.setup_driver()
+        driver = methods.setup_driver()
         r = []
         
-        has_privacy = environment.check_privacy_policy(driver)
+        has_privacy = methods.check_privacy_policy(driver)
         r.append({
             'check': 'Política de privacidade',
             'passed': has_privacy
         })
         
-        has_cookie_option = environment.check_cookie_banner(driver)
+        has_cookie_option = methods.check_cookie_banner(driver)
         r.append({
             'check': 'Opção de recusar cookies',
             'passed': has_cookie_option
@@ -40,7 +40,7 @@ def run_crawler():
         r.append(False)
         
         consent_locator = (By.XPATH, "//button[contains(.,'Accept')]")
-        consent_check = environment.check_consent_before_cookies(consent_locator, driver)
+        consent_check = methods.check_consent_before_cookies(consent_locator, driver)
         r.append({
             'check': 'Coleta de cookies somente após consentimento',
             'passed': consent_check if consent_check is not None else False
